7297b307779733b63609d1fb1047ee0ba39e7ea4
Tom Kornblit <tom.kornblit@gmail.com>
2016-05-06 12:06:42 -0700

    Updated to support folders.

--- tasks/mongoimport.js
+++ tasks/mongoimport.js
@@ -1,25 +1,10 @@
 var async = require('async');
+var walkSync = require('walk-sync');
+var path = require("path");
 var TASK_ERROR = 3;
 
-module.exports = function(grunt) {
-  grunt.registerTask("mongoimport", "Grunt task for importing data into mongodb", function() {
+var _getArgs = function(options, collection, file) {
 
-  var done = this.async();
-  var options = this.options();
-
-  if (!options) {
-    grunt.warn(new Error('Options not found.', TASK_ERROR));
-  }
-
-  if (!options.collections) {
-    grunt.warn(new Error('Collections option not found.', TASK_ERROR));
-  }
-
-  if (!Array.isArray(options.collections)) {
-    grunt.warn(new Error('Collections must be an array'), TASK_ERROR);
-  }
-
-  async.eachSeries(options.collections, function(collection, callback){
     var args = [];
 
     if (options.db) args.push('--db=' + options.db);
@@ -34,33 +19,115 @@ module.exports = function(grunt) {
     if (collection.type === 'csv' || collection.type === 'tsv') {
         if (collection.headerLine) args.push('--headerline');
     }
-    if (collection.file) args.push('--file=' + collection.file);
     if (collection.fields) args.push('--fields=' + collection.fields);
     if (collection.upsertFields) args.push('--upsertFields=' + collection.upsertFields);
     if (collection.jsonArray) args.push('--jsonArray');
     if (collection.upsert) args.push('--upsert');
     if (collection.drop) args.push('--drop');
 
-    var child = grunt.util.spawn({
-      cmd: 'mongoimport',
-          args: args,
-          opts: {
-            stdio: 'inherit'
-          }
-        },
-        function (error, result) {
-          if (error) {
-            grunt.log.error(result.stderr);
-            callback();
-          }
-          grunt.log.writeln(result.stdout);
-          callback();
-        });
-      },
-      function(err){
-        if (err) done(false);
-        done();
-      }
-    );
-  });
+    args.push('--file=' + file);
+
+    return args;
+
+};
+
+module.exports = function(grunt) {
+
+    grunt.registerTask("mongoimport", "Grunt task for importing data into mongodb", function() {
+
+        var done = this.async();
+        var options = this.options();
+
+        if (!options) {
+            grunt.warn(new Error('Options not found.', TASK_ERROR));
+        }
+
+        if (!options.collections) {
+            grunt.warn(new Error('Collections option not found.', TASK_ERROR));
+        }
+
+        if (!Array.isArray(options.collections)) {
+            grunt.warn(new Error('Collections must be an array'), TASK_ERROR);
+        }
+
+        async.eachSeries(options.collections, function(collection, callback) {
+
+                if (collection.folder) {
+
+                    var files = walkSync(collection.folder)
+
+                    async.eachSeries(files, function(file, cb) {
+
+                        if (path.extname(file) === ".json") {
+
+                            file = collection.folder + "/" + file;
+
+                            var args = _getArgs(options, collection, file);
+
+                            var child = grunt.util.spawn({
+                                    cmd: 'mongoimport',
+                                    args: args,
+                                    opts: {
+                                        stdio: 'inherit'
+                                    }
+                                },
+                                function(error, result) {
+
+                                    if (error) {
+                                        grunt.log.error(result.stderr);
+                                        cb();
+                                    }
+
+
+                                    grunt.log.writeln("Imported file: %s", file);
+                                    cb();
+                                });
+
+                        } else {
+
+
+                            cb();
+
+                        }
+
+                    }, function(err) {
+
+                        callback();
+
+                    })
+
+
+                } else if (collection.file) {
+
+                    var args = _getArgs(collection, collection.file);
+
+                    var child = grunt.util.spawn({
+                            cmd: 'mongoimport',
+                            args: args,
+                            opts: {
+                                stdio: 'inherit'
+                            }
+                        },
+                        function(error, result) {
+                            if (error) {
+                                grunt.log.error(result.stderr);
+                                callback();
+                            }
+                            grunt.log.writeln(result.stdout);
+                            callback();
+                        });
+
+                }
+
+
+            },
+            function(err) {
+                if (err) done(false);
+                done();
+            }
+        );
+    });
+
+
+
 };
